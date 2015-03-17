945e20a2353f7814e0ab1d897cdd3cddb9694d29
Enrico Carlesso <enricocarlesso@gmail.com>
2015-03-17 22:35:09 +0100

    Implemented :log_level
    As per mattbrictson/airbrussh#3, this commit adds :log_level to
    Airbrussh configuration, allowing stdout to be enabled if needed.

--- lib/airbrussh/configuration.rb
+++ lib/airbrussh/configuration.rb
@@ -1,12 +1,13 @@
 module Airbrussh
   class Configuration
-    attr_accessor :log_file, :monkey_patch_rake, :color, :truncate
+    attr_accessor :log_file, :monkey_patch_rake, :color, :truncate, :log_level
 
     def initialize
       self.log_file = nil
       self.monkey_patch_rake = false
       self.color = :auto
       self.truncate = :auto
+      self.log_level = :info
     end
   end
 end
