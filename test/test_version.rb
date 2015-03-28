af3b8a5068c1e49a278f43c1fce2e80e05f2be30
Matt Brictson <matt@mattbrictson.com>
2015-03-27 18:21:20 -0700

    Clean up existing unit test

--- test/test_version.rb
+++ test/test_version.rb
@@ -1,11 +1,7 @@
 require "minitest_helper"
 
-class TestAirbrussh < Minitest::Test
+class TestVersion < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Airbrussh::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
