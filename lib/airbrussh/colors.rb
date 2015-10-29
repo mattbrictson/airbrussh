473222e6dda0eeaa602461fa31eec22545bdb0c7
Matt Brictson <matt@mattbrictson.com>
2015-10-28 20:04:18 -0700

    Add more color names (from SSHKit::Colors)

--- lib/airbrussh/colors.rb
+++ lib/airbrussh/colors.rb
@@ -3,12 +3,28 @@ module Airbrussh
   # any external dependencies.
   module Colors
     ANSI_CODES = {
-      :red    => 31,
-      :green  => 32,
-      :yellow => 33,
-      :blue   => 34,
-      :gray   => 90
+      :black   => 30,
+      :red     => 31,
+      :green   => 32,
+      :yellow  => 33,
+      :blue    => 34,
+      :magenta => 35,
+      :cyan    => 36,
+      :white   => 37,
+      :gray    => 90,
+      :light_black   => 90,
+      :light_red     => 91,
+      :light_green   => 92,
+      :light_yellow  => 93,
+      :light_blue    => 94,
+      :light_magenta => 95,
+      :light_cyan    => 96,
+      :light_white   => 97
     }.freeze
+
+    def self.names
+      ANSI_CODES.keys
+    end
 
     # Define red, green, blue, etc. methods that return a copy of the
     # String that is wrapped in the corresponding ANSI color escape
